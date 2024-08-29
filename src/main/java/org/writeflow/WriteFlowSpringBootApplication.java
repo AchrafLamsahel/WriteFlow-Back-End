a7d4f39fe11b49d55c9639ffb5749053f3dbe5b9
AchrafLamsahel <Achraflamsahel1@gmail.com>
2024-08-29 16:38:17 +0100

    First Commit

---
+++ src/main/java/org/writeflow/WriteFlowSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.writeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteFlowSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriteFlowSpringBootApplication.class, args);
+	}
+
+}
